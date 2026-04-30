d1716497a69d4e327fca6ad33836ace088edb52b
Prasanna kumar Kommiri <142135618+prassu02@users.noreply.github.com>
2026-04-30 22:32:14 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
